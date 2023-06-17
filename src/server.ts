6ba4f043bbb87520c1e5639d82f918a9a3e3c46e
Rafael Tiburcio <rafael.tiburcio@live.com>
2023-06-16 23:04:12 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .where('amount', 500)
+    .select('*')
+  return transaction
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Http running')
+  })
